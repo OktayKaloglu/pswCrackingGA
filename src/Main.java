5e9c128c1b624aef55b956f274a12801dd6f057a
HarpaxPantensus <kaloglu.oktay@gmail.com>
2022-05-13 02:33:29 +0300

    Captain's Log, Stardate 202205.13. Cmdr. Data crated a password cracking program for the tampered research computer. Our guest had completed his work on the L 98-59 star system. We have been founding facts that has undeniable similarities to the life's process on earth. We have been learning so much about how the life started at our own.

--- src/Main.java
+++ src/Main.java
@@ -8,67 +8,56 @@ public class Main {
 
     public static void main(String[] Args){
         String psw="Deep Learning 2022";
-        PswCracker cracker = new PswCracker(64, psw, 5);
         ArrayList<String[]> popsData;
         NumberFormat formatter = new DecimalFormat("#0.0000");
 
         long timeTakes=0;
-        for(int i =0;i<3;i++) {
+        for(int i =0;i<4;i++) {
+            PswCracker cracker = new PswCracker(128, psw, 10);
+
             long startTime = System.nanoTime();
             popsData= cracker.crackThePSW();
             long endTime = System.nanoTime();
             long totalTime = endTime - startTime;
-            timeTakes+=((totalTime) / 1000000d);
             //System.out.println("Generation    "+"Best Chromosome    "+"Generation's Fitness Mean");
-            for(int j =0;j<popsData.size();j++){
-                System.out.println("Generation: "+j+"  Fitness Mean: "+popsData.get(j)[0]+"  Best Chromosome: "+popsData.get(j)[1]);
+            if(i>0) {
+                timeTakes+=((totalTime) / 1000000d);
+                for (int j = 0; j < popsData.size(); j++) {
+                    System.out.println("Generation: " + j + "  Fitness Mean: " + popsData.get(j)[0] + "  Best Chromosome: " + popsData.get(j)[1]);
+                }
+
+                System.out.println("Execution time is " + formatter.format((totalTime) / 1000000d) + " mili seconds");
+                System.out.println(" ");
+                System.out.println(" ");
+                System.out.println(" ");
             }
-            System.out.println("Execution time is " + formatter.format((totalTime)/1000000d) + " mili seconds");
-            System.out.println(" ");
-            System.out.println(" ");
-            System.out.println(" ");
         }
         System.out.println("Mean execution time is " + formatter.format((timeTakes) / 3) + " micro seconds");
-
-        psw="DeepLearningDeepLearning";
-        cracker = new PswCracker(64, psw, 5);
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println(" ");
+        psw="DeepLearning";
         timeTakes=0;
-        for(int i =0;i<3;i++) {
+        for(int i =0;i<4;i++) {
+            PswCracker cracker = new PswCracker(128, psw, 10);
+
             long startTime = System.nanoTime();
             popsData= cracker.crackThePSW();
             long endTime = System.nanoTime();
             long totalTime = endTime - startTime;
-            timeTakes+=((totalTime) / 1000000d);
-            //System.out.println("Generation    "+"Best Chromosome    "+"Generation's Fitness Mean");
-            for(int j =0;j<popsData.size();j++){
-                System.out.println("Generation: "+j+"  Fitness Mean: "+popsData.get(j)[0]+"  Best Chromosome: "+popsData.get(j)[1]);
-            }
-            System.out.println("Execution time is " + formatter.format((totalTime)/1000000d) + " mili seconds");
-            System.out.println(" ");
-            System.out.println(" ");
-            System.out.println(" ");
-        }
+            
+            if(i>0) {
+                timeTakes+=((totalTime) / 1000000d);
+                for (int j = 0; j < popsData.size(); j++) {
+                    System.out.println("Generation: " + j + "  Fitness Mean: " + popsData.get(j)[0] + "  Best Chromosome: " + popsData.get(j)[1]);
+                }
+
+                System.out.println("Execution time is " + formatter.format((totalTime) / 1000000d) + " mili seconds");
+                System.out.println(" ");
+                System.out.println(" ");
+                System.out.println(" ");
+            }        }
         System.out.println("Mean execution time is " + formatter.format((timeTakes) / 3) + " micro seconds");
-
-
-
-
-        /*
-        SplittableRandom rnd= new SplittableRandom();
-
-         int genePoolStart=32;
-         int genePollEnd=127;
-        //these 2 used for ascii table
-
-        char[] turkishLetters={'Ç','ç','Ö','ö','Ü','ü','İ','ı','Ğ','ğ','Ş','ş'};
-        int upperbound=genePollEnd+turkishLetters.length;
-        for (int i =0;i<10000;i++){
-            int rndChar=rnd.nextInt(genePoolStart,genePollEnd+turkishLetters.length);
-            if(rndChar>=genePollEnd)
-                System.out.println(rndChar-genePollEnd);
-        }
-        */
-
 
 
     }
