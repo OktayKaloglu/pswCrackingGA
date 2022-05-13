7a613987158ef038afb470a8766b21c8e5f569e1
HarpaxPantensus <kaloglu.oktay@gmail.com>
2022-05-13 05:59:17 +0300

    Captain's Log, Stardate 202205.13. Cmdr. Data crated a password cracking program for the tampered research computer. Our guest had completed his work on the L 98-59 star system. We have been founding facts that has undeniable similarities to the life's process on earth. We have been learning so much about how the life started at our own.

--- src/Main.java
+++ src/Main.java
@@ -10,8 +10,9 @@ public class Main {
         String psw="Deep Learning 2022";
         ArrayList<String[]> popsData;
         NumberFormat formatter = new DecimalFormat("#0.0000");
-
+        NumberFormat formatter2 = new DecimalFormat("#0.00");
         long timeTakes=0;
+        long meanGeneration=0;
         for(int i =0;i<4;i++) {
             PswCracker cracker = new PswCracker(128, psw, 10);
 
@@ -25,19 +26,22 @@ public class Main {
                 for (int j = 0; j < popsData.size(); j++) {
                     System.out.println("Generation: " + j + "  Fitness Mean: " + popsData.get(j)[0] + "  Best Chromosome: " + popsData.get(j)[1]);
                 }
-
-                System.out.println("Execution time is " + formatter.format((totalTime) / 1000000d) + " mili seconds");
+                meanGeneration+=popsData.size()-1;
+                System.out.println("Execution time is " + formatter.format((totalTime) / 1000000d) + " milli seconds");
                 System.out.println(" ");
                 System.out.println(" ");
                 System.out.println(" ");
             }
         }
-        System.out.println("Mean execution time is " + formatter.format((timeTakes) / 3) + " mili seconds");
+        System.out.println("Mean execution time is " + formatter.format((timeTakes) / 3) + " milli seconds");
         System.out.println(" ");
+        System.out.println("Mean generation is " + formatter2.format((meanGeneration) / 3));
+
         System.out.println(" ");
         System.out.println(" ");
         psw="DeepLearning";
         timeTakes=0;
+        meanGeneration=0;
         for(int i =0;i<4;i++) {
             PswCracker cracker = new PswCracker(128, psw, 10);
 
@@ -51,14 +55,14 @@ public class Main {
                 for (int j = 0; j < popsData.size(); j++) {
                     System.out.println("Generation: " + j + "  Fitness Mean: " + popsData.get(j)[0] + "  Best Chromosome: " + popsData.get(j)[1]);
                 }
-
-                System.out.println("Execution time is " + formatter.format((totalTime) / 1000000d) + " mili seconds");
+                meanGeneration+=popsData.size()-1;
+                System.out.println("Execution time is " + formatter.format((totalTime) / 1000000d) + " milli seconds");
                 System.out.println(" ");
                 System.out.println(" ");
                 System.out.println(" ");
             }        }
-        System.out.println("Mean execution time is " + formatter.format((timeTakes) / 3) + " mili seconds");
-
+        System.out.println("Mean execution time is " + formatter.format((timeTakes) / 3) + " milli seconds");
+        System.out.println("Mean generation is " + formatter2.format((meanGeneration) / 3));
 
     }
 }
